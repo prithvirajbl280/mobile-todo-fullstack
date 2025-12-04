4c1fb657b6ead652473e87455049141ad868a6ed
Prithvi Raj <prithvirajbl14@gmail.com>
2025-12-05 01:48:39 +0530

    wip: testing navigation and app setup

--- mobile/mobile-expo/App.tsx
+++ mobile/mobile-expo/App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello! App is working!</Text>
-    </View>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
